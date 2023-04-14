7897df8feb8c91cd3dc6e837660991fa94162402
Ming Jun <evan.zhangmingjun@gmail.com>
2023-04-14 18:31:13 +0800

    added home page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Providers } from "./providers";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+        <Providers>
+          <Main />
+          <NextScript />
+        </Providers>
       </body>
     </Html>
-  )
+  );
 }
