442914df5f16f1d4ae7d4e8fda9ea863e73bdd50
Ming Jun <evan.zhangmingjun@gmail.com>
2023-08-19 19:32:08 +0800

    fix: stop vid from playing in mobile view

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -6,17 +6,18 @@ import Navbar from './Navbar';
 
 export default function Layout({ children }) {
     return (
-        <div className="relative flex flex-col w-full">
-            <div className="max-[1350px]:invisible flex flex-col min-h-screen max-h-screen w-full overflow-hidden absolute top-0 left-0">
+        <div className="flex flex-col w-full">
+            <div className="max-[1350px]:hidden flex flex-col min-h-screen max-h-screen w-full overflow-hidden">
                 <Head>
                     <title>Scribr: The AI YouTube Assistant</title>
                 </Head>
                 <header className="flex-none bg-white">
                     <Navbar showSearchBar={true} />
                 </header>
-                {children}
+                {/* Below is just a temporary fix, the window width is only compared once when the component is first mounted and it does not track the changes in window width if the window is resized later on */}
+                {window.innerWidth > 1350 ? children : null}
             </div>
-            <div className="min-[1350px]:invisible flex flex-col min-h-screen max-h-screen w-full overflow-hidden absolute top-0 left-0 p-8 justify-center bg-slate-50">
+            <div className="min-[1350px]:hidden flex flex-col min-h-screen max-h-screen w-full overflow-hidden p-8 justify-center bg-slate-50">
                 <div className="flex flex-col items-center">
                     <Image src={Dreamer} alt="dreamer" className="w-4/5 md:w-1/2 mb-8" />
                     <h1 className="text-center text-3xl font-bold mb-4">{'Working on it!'}</h1>
