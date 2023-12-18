be9b675971bd6c74c0e50790244bf220e7d5fa88
Ming Jun <evan.zhangmingjun@gmail.com>
2023-12-18 23:58:08 +0800

    chore: remove unused metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,6 @@ import '@/styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
-export const metadata = {
-    title: 'Scibr',
-    description: 'AI YouTube Assistant',
-};
-
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider>
